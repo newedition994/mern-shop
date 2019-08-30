b37bc5cd3ef6497063228c99e1c4f345246de86e
Breyanna Vass <breyannavass@gmail.com>
2019-08-30 18:01:06 -0400

    importing connect and additem actions

--- client/src/components/itemModal.js
+++ client/src/components/itemModal.js
@@ -1,3 +1,11 @@
 import React, { Component } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input } from 'reactstrap';
 
+import { connect } from 'react-redux'
+import { addItem } from '../actions/itemActions'
+
+class ItemModal extends Component {
+
+}
+
+export default connect()(ItemModal);
