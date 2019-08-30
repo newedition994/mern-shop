e2afd3ef73300434d51903975eb4e9778228c75a
Breyanna Vass <breyannavass@gmail.com>
2019-08-30 18:46:03 -0400

    Connecting to the backend

--- client/src/components/itemModal.js
+++ client/src/components/itemModal.js
@@ -3,7 +3,6 @@ import { Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input } 
 
 import { connect } from 'react-redux'
 import { addItem } from '../actions/itemActions'
-import uuid from 'uuid'
 
 class ItemModal extends Component {
     state = {
@@ -25,7 +24,6 @@ class ItemModal extends Component {
         e.preventDefault();
 
         const newItem = {
-            id: uuid(),
             name: this.state.name
         }
 
