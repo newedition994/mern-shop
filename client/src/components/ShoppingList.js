bb05939c258159e6abe78b1fb94fa03434409a6e
Breyanna Vass <breyannavass@gmail.com>
2019-08-30 17:51:16 -0400

    Fixing the delete action and adding item modal

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuid from 'uuid';
 import { connect } from 'react-redux';
-import { getItems } from '../actions/itemActions';
+import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
 
 class ShoppingList extends Component {
@@ -20,33 +19,22 @@ class ShoppingList extends Component {
         this.props.getItems();
     }
 
+    onDeleteClick = (id) => {
+        this.props.deleteItem(id)
+    }
+
     render() {
 
         const { items } = this.props.item;
 
         return (
             <Container>
-                <Button
-                    color="dark"
-                    style={{ marginBottom: '2rem' }}
-                    onClick={() => {
-                        const name = prompt('Enter item');
-                        if (name) {
-                            this.setState(state => ({
-                                items: [...state.items, { id: uuid(), name }]
-                            }));
-                        }
-                    }}>Add Item</Button>
                 <ListGroup>
                     <TransitionGroup className="shopping-list">
                         {items.map(({ id, name }) => (
                             <CSSTransition key={id} timeout={500} classNames="fade">
                                 <ListGroupItem>
-                                    <Button className="remove-btn" color="danger" size="sm" onClick={() => {
-                                        this.setState(state => ({
-                                            items: state.items.filter(item => item.id !== id)
-                                        }))
-                                    }}
+                                    <Button className="remove-btn" color="danger" size="sm" onClick={this.onDeleteClick.bind(this, id)}
                                     >&times;</Button>
                                     {name}
                                 </ListGroupItem>
@@ -68,4 +56,4 @@ const mapStateToProp = (state) => ({
     item: state.item
 });
 
-export default connect(mapStateToProp, { getItems })(ShoppingList);
+export default connect(mapStateToProp, { getItems, deleteItem })(ShoppingList);
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -10,12 +10,17 @@ const initialState = {
     ]
 }
 
-export default function (state = initialState, actiion) {
-    switch (actiion.type) {
+export default function (state = initialState, action) {
+    switch (action.type) {
         case GET_ITEMS:
             return {
                 ...state
-            }
+            };
+        case DELETE_ITEM:
+            return {
+                ...state,
+                items: state.items.filter(item => item.id !== action.payload)
+            };
         default:
             return state;
     }
