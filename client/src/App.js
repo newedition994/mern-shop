e974e1c2a84bf7e1707fa352269befbf3174de65
Breyanna Vass <breyannavass@gmail.com>
2019-08-30 18:14:35 -0400

    Adding form body to modal and appjs

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/itemModal';
+import { Container } from 'reactstrap'
+
+
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -12,7 +16,10 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <AppNavbar />
-        <ShoppingList />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
       </div>
     </Provider>
   );
