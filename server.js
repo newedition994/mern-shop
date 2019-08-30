8431d7781da52e3158d71c104b7b976c9c790304
Breyanna Vass <breyannavass@gmail.com>
2019-08-30 18:57:24 -0400

    Add server and packagejson

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path')
 
 const items = require('./routes/api/items')
 
@@ -19,6 +20,15 @@ mongoose.connect(db)
 
 // Use Routes
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000;
 
